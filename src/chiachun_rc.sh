ddec8ffdeb1f5e7afbf6c172c05485fe81b48912
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2024-01-27 18:36:33 +0800

    Print Zen Quotes

--- src/chiachun_rc.sh
+++ src/chiachun_rc.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+run_zen_quotes() {
+    local py_path
+    py_path="$(get_venv_bin_path ~/'repo/_packages')/python"
+    local script_path=~/'repo/helper-codes/src/zen_quotes/zen_quotes.py'
+
+    "$py_path" "$script_path"
+}
+
 source_bash_alias() {
     if [[ -f ~/'repo/helper-codes/src/bash_alias.sh' ]]; then
         # shellcheck source=/dev/null
@@ -18,10 +26,29 @@ source_git_hooks_ci() {
     fi
 }
 
+source_py_sh() {
+    local py_sh_path=~/'repo/git-hooks/src/py.sh'
+
+    if [[ ! -f "$py_sh_path" ]]; then
+        echo "$py_sh_path not found"
+        return 1
+    fi
+
+    # shellcheck source=/dev/null
+    source "$py_sh_path"
+}
+
 main() {
     echo '##################################################'
+    if ! source_py_sh; then
+        return 1
+    fi
+
     source_bash_alias
     source_git_hooks_ci
+
+    echo ''
+    run_zen_quotes
     echo '##################################################'
 }
 
