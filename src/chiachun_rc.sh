eec11e2eae11cf4c693a77755df5ab02cab8cd58
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2024-02-27 23:08:36 +0800

    Move set_django_env_var() to git-hooks

--- src/chiachun_rc.sh
+++ src/chiachun_rc.sh
@@ -17,30 +17,6 @@ run_zen_quotes() {
     "$py_path" "$script_path"
 }
 
-set_django_env_var() {
-    local django_dir
-    if ! django_dir="$(get_env_value \
-        "$(get_first_env_var './.env' 'MY_DJANGO_PROJECT')")"; then
-        return 1
-    fi
-
-    local env_line
-    if ! env_line="$(grep --max-count=1 'DJANGO_SETTINGS_MODULE' \
-        "$django_dir/manage.py")"; then
-        return 1
-    fi
-
-    [[ "$env_line" =~ [^'"']+'"'([^'"']+)'"'[^'"']+'"'([^'"']+)'"' ]]
-    if [[ "${#BASH_REMATCH[@]}" -ne 3 ]]; then
-        return 1
-    fi
-    local key="${BASH_REMATCH[1]}"
-    local value="${BASH_REMATCH[2]}"
-
-    export "$key"="$value"
-    echo "Set $key to $value"
-}
-
 source_bash_alias() {
     local repo_dir="$1"
 
